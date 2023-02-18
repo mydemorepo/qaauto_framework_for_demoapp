6181c999ef5284b61167f4608ae9b13093c78c7f
Adam Vasyliuta <adamvasyliuta@gmail.com>
2023-02-18 16:57:46 +0200

    Added file download check.

--- modules/api/clients/my_client.py
+++ modules/api/clients/my_client.py
@@ -6,14 +6,21 @@ class MyClient():
         self.server_url = server_url
         self.cureent_response = None
     
-    def post_content(self, route, request_data):
+    def post_content(self, route, request_data, cookie = None):
         self.cureent_response = requests.post(self.server_url + route, 
-                                         data = request_data)
+                                         data = request_data, cookies=cookie)
         return self.cureent_response.json()
    
-    def get_content(self, route):
-        self.cureent_response = requests.get(self.server_url + route)
+    def get_content(self, route, cookie = None):
+        self.cureent_response = requests.get(self.server_url + route, cookies=cookie)
         return self.cureent_response.json()
     
     def get_cookies(self):
-        return self.cureent_response.cookies    
+        return self.cureent_response.cookies
+    
+    def get_headers(self):
+        return dict(self.cureent_response.headers)
+    
+    def get_file(self, route, cookie = None):
+        self.cureent_response = requests.get(self.server_url + route, cookies=cookie)
+        return self.cureent_response
--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,4 +1,5 @@
 import pytest
+import csv
 
 
 @pytest.mark.test    
@@ -43,15 +44,38 @@ def test_check_column_selection (database, client):
             
     for cookie in client.get_cookies():
         query_string = cookie.value
-            
-   
-    print(query_string[1:-1].replace('\\054', ','))   
-
- 
+                
     db_table_content = database.get_result(query_string[1:-1].replace('\\054', ','))
             
     for resp_row, db_row in zip(resp_table_content, db_table_content):
         for resp_cell, db_cell in zip(resp_row, db_row):
             assert str(resp_row[resp_cell]) == str(db_row[db_cell])
+
+@pytest.mark.test                    
+def test_download_csv_file (database, client):
+    name = 'offices'
+    request_data = {'city': 'on', 'country': 'on'}
     
+    response = client.post_content(f'/api/json/{name}', request_data)   
+    #response = client.get_content(f'/api/json/{name}')
     
+    cookies  = client.get_cookies()
+    
+    csv_file = client.get_file(f'/download/{name}',  cookies)
+    
+    frecord = open(client.get_headers()['Content-Disposition'][21:], 'wt', newline='')
+    frecord.write(csv_file.text)
+    frecord.close()
+    
+    with open(client.get_headers()['Content-Disposition'][21:], 'rt') as freading:
+        creading = csv.DictReader(freading)
+        
+        query_string = None
+        for cookie in cookies:
+            query_string = cookie.value
+                
+        db_table_content = database.get_result(query_string[1:-1].replace('\\054', ','))
+        
+        for file_row, db_row in zip(creading, db_table_content):
+            for file_row_cell, db_row_cell in zip(file_row, db_row):
+                assert file_row[file_row_cell] == db_row[db_row_cell] 
